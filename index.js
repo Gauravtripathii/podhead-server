2fbc648af124a7235c8c8eb63c8589e78969cad5
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-22 21:39:28 +0530

    route setup

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import userRouter from "./routes/user.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/user", userRouter);
 
 const CONNECTION_URL =
   "mongodb+srv://podheadUser:abcde@cluster0.zfkjpkm.mongodb.net/?retryWrites=true&w=majority";
