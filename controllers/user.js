f680d9e56986e2e9c263b1078be084009321e016
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-07-23 20:57:04 +0530

    create user method

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,25 @@
 //Step 2
-import UserData from "../models/users.js";  // import the UserData from the path to use it here
+import UserData from "../models/users.js"; // import the UserData from the path to use it here
 
-export const getAllUsers = async (req, res) => { // export the constant getAllUsers to request Handler Function
-  try {                                               // This try and catch will help in smooth handling of error
-    const userData = await UserData.find(); // .find() will provide all UserData 
+export const getAllUsers = async (req, res) => {
+  // export the constant getAllUsers to request Handler Function
+  try {
+    // This try and catch will help in smooth handling of error
+    const userData = await UserData.find(); // .find() will provide all UserData
     res.status(200).json(userData);
   } catch (error) {
-    res.status(404).json(error.message);
+    res.status(404).json({error: error.message});
+  }
+};
+
+export const createUser = async (req, res) => {
+  const user = req.body;
+  const newUser = new UserData(user);
+
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 //Step 3
-import { getAllUsers } from "../controllers/user.js";  // import all the getAllUsers from the defined path
+import { getAllUsers, createUser } from "../controllers/user.js";  // import all the getAllUsers from the defined path
 
 import express from "express";       // import the express module
 
 const router = express.Router();  // creating a router instance using Router() methodd belongs to express
 
 router.get("/", getAllUsers);       // specify get method for the root /user specified in the index.js and passing getAllUser handler function.
+router.post("/", createUser);
 
 export default router; //wxported to be used anywhere
