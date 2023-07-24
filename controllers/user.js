d67d129d4b7e404d397a97074286b8eb694bc288
VATSAL77726 <vatsalsaxenais@gmail.com>
2023-07-24 23:15:09 +0530

    comment controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,30 +1,30 @@
-//Step 2
+//Step 2  Create all the different methods CRUD
 import UserData from "../models/users.js"; // import the UserData from the path to use it here
 
-export const getAllUsers = async (req, res) => {
-  // export the constant getAllUsers to request Handler Function
-  try {
-    // This try and catch will help in smooth handling of error
-    const userData = await UserData.find(); // .find() will provide all UserData
+export const getAllUsers = async (req, res) => {      // object for accessing all userData, export the constant getAllUsers to use this Handler Function
+                                                      
+  try {                                               // This try and catch will help in smooth handling of error or success
+                                                      
+    const userData = await UserData.find();           // .find() will provide all UserData stored in db
     res.status(200).json(userData);
   } catch (error) {
     res.status(404).json({error: error.message});
   }
 };
 
-export const createUser = async (req, res) => {
+export const createUser = async (req, res) => {         //  constant to store object for creating an user 
   const user = req.body;
   const newUser = new UserData(user);
 
   try {
-    await newUser.save();
+    await newUser.save();           // async stands for asynchronous function that await for the process to finish and then moves forward 
     res.status(201).json(newUser);
   } catch (error) {
     res.status(409).json({ error: error.message });
   }
 };
 
-export const getUser = async (req, res) => {
+export const getUser = async (req, res) => {        //  Get a specific user using id
   const {id} = req.params;
   try {
     const userData = await UserData.findById(id);
@@ -34,7 +34,7 @@ export const getUser = async (req, res) => {
   }
 }
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res) => {   //  Delete an specific user
   const {id} = req.params;
   try {
     await UserData.findByIdAndRemove(id);
@@ -44,10 +44,10 @@ export const deleteUser = async (req, res) => {
   }
 }
 
-export const updateUser = async (req, res) => {
-  const {id} = req.params;
-  const { name, email, age, liked_podcasts, genre, listners,podcast_created, password, date_of_creation } = req.body;
-  const updatedUser = { name, email, age, liked_podcasts, genre, listners,podcast_created, password, date_of_creation, _id : id };
+export const updateUser = async (req, res) => {     //   update an user data
+  const {id} = req.params;                          //   Accessing  the id from parameter of the url (shorthand way)
+  const { name, email, age, liked_podcasts, genre, listners,podcast_created, password, date_of_creation } = req.body;                                             //  Getting hold of on each info of the user  thru body came thru req
+  const updatedUser = { name, email, age, liked_podcasts, genre, listners,podcast_created, password, date_of_creation, _id : id };                     // updating the req body data into the database basically commiting 
   try {
     await UserData.findByIdAndUpdate(id, updatedUser, {new : true} );
     res.status(200).json(updatedUser);
